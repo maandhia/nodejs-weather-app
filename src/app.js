7b4bd2fef0f0f1659161f3ea9d455e58b6bcbbc6
Maan Sidiki <maandhia2015@gmail.com>
2020-01-28 15:49:21 -0500

    destructuring an object in the function call to geocode

--- src/app.js
+++ src/app.js
@@ -59,23 +59,25 @@ app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({ error: "please enter an address" });
   }
-  geocode(req.query.address, (error, data) => {
-    if (error) {
-      return res.send({ error });
-    }
-
-    const { latitude, longitude, location } = data;
-    forecast(latitude, longitude, location, (error, data) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
 
-      res.send({
-        location: location,
-        forecast: data
+      forecast(latitude, longitude, location, (error, data) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          location: location,
+          forecast: data
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/help/*", (req, res) => {
